8f7de17c06d4a8178135c99cf1d11d296f3884f4
rhairy <randall.harrison91@gmail.com>
2016-12-24 13:12:20 -0800

    Updated 1-18.c

--- src/1-18.c
+++ src/1-18.c
@@ -4,28 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IN 1
-#define OUT 0
+#include "fun.h"
+
 
 int main(int argc, char *argv[])
 {
 	char *str = NULL;
 	int res;
 	size_t len = 0;
-	int i;
 
 	while ( (res = getline(&str, &len, stdin)) != -1 ) {
-		// Set i equal to the last element of the string, not counting the null terminator..
-		i = res;
-		// Work backwards to find the first whitespace char.
-		while (i > 0 && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\0')) {
-			i--;
-		}
-		// If a non-whitespace char was found, set the char after it to the null terminator.
-		if (i > 0) {
-			i = i + 1;
-			str[i] = '\0';	
-		}
+		rmtrail(str, res);
 		printf("%s\n", str);
 		free(str);
 		str = NULL;
