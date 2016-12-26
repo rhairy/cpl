ebb16e38320138a15099211fb57785c056a2bcab
rhairy <randall.harrison91@gmail.com>
2016-12-25 20:11:52 -0800

    Updated Exercise 1-20.

--- src/1-20.c
+++ src/1-20.c
@@ -2,21 +2,78 @@
  * of blanks to space to the next tab stop.  Assume a fixed set of tab stops, say 
  * every n columns.  Should n be a variable or a symbolic parameter? */
 
+#include <errno.h>
 #include <stdio.h>
-
-#define N 1
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
 	int c;
-	while ( (c = getchar()) != EOF ) {
-		if (c == '\t') {
-			for (unsigned i = 0; i < N; i++) {
-				putchar(' ');	
+	int n = 4;
+	char *inpath = NULL;
+	char *outpath = NULL;
+	FILE *infile = NULL;
+	FILE *outfile = NULL;
+
+	while ( (c = getopt(argc, argv, "i:n:o:")) != -1 ) {
+		switch (c) {
+		case 'i':
+			inpath = optarg;
+			break;
+		case 'n':
+			n = atoi(optarg);
+			break;
+		case 'o':
+			outpath = optarg;
+			break;
+		case '?':
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (inpath == NULL) {
+		infile = stdin;
+	} else {
+		infile = fopen(inpath, "r");
+		if (infile == NULL) {
+			perror("fopen: ");
+			return -1;
+		}
+	} 
+
+	if (outpath == NULL) {
+		outfile = stdout;
+	} else {
+		outfile = fopen(outpath, "w");
+		if (outfile == NULL) {
+			perror("fopen: ");
+			fclose(infile);
+			return -1;
+		}
+	}	
+	if (n <= 0) {
+		n = 4;
+	} 
+
+	int d;
+	while ( (d = getc(infile)) != EOF ) {
+		if (d == '\t') {
+			for (unsigned i = 0; i < n; i++) {
+				putc(' ', outfile);
 			}
 		} else {
-			putchar(c);
+			putc(d, outfile);
 		}
+	} 
+	
+	if (infile != stdin) {
+		fclose(infile);
+	}
+	if (outfile != stdout) {
+		fclose(outfile);
 	}
 	return 0;
 }
