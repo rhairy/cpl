e4378d7736f70bbf3c6b2170a877282f1c1bdf62
rhairy <randall.harrison91@gmail.com>
2016-12-25 17:17:36 -0800

    Updated Exercise 1-18.c

--- src/1-18.c
+++ src/1-18.c
@@ -1,21 +1,67 @@
 /* Write a program to remove trailing blanks and tabs from each line of
  * input, and to delete entirely blank lines. */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "fun.h"
 
 
 int main(int argc, char *argv[])
 {
+	int c;
+	char *inpath = NULL;
+	char *outpath = NULL;
+	FILE *infile = NULL;
+	FILE *outfile = NULL;
+
+	while ( (c = getopt(argc, argv, "i:o:")) != -1 ) {
+		switch (c) {
+		case 'i':
+			inpath = optarg;
+			break;
+		case 'o':
+			outpath = optarg;
+			break;
+		case '?':
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (inpath == NULL) {
+		infile = stdin;
+	} else {
+		infile = fopen(inpath, "r");
+		if (infile == NULL) {
+			perror("fopen: ");
+			return -1;
+		}
+	} 
+
+	if (outpath == NULL) {
+		outfile = stdout;
+	} else {
+		outfile = fopen(outpath, "w");
+		if (outfile == NULL) {
+			perror("fopen: ");
+			fclose(infile);
+			return -1;
+		}
+	}
+
 	char *str = NULL;
 	int res;
 	size_t len = 0;
+	size_t rm_len;
 
-	while ( (res = getline(&str, &len, stdin)) != -1 ) {
-		rmtrail(str, res);
-		printf("%s\n", str);
+	while ( (res = getline(&str, &len, infile)) != -1 ) {
+		if ( (rm_len = rmtrail(str, res)) > 0) {
+			fwrite(str, sizeof(char), rm_len, outfile);
+		}
 		free(str);
 		str = NULL;
 	}
